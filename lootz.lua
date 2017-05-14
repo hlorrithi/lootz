01e179390d2f1d040f45511dfee967be74c10055
hlorrithi <hlorrithi@gizmo>
2017-05-14 14:59:53 +0200

    slightly improved lazy shit
    Signed-off-by: hlorrithi <hlorrithi@gizmo>

--- lootz.lua
+++ lootz.lua
@@ -2,33 +2,50 @@ SLASH_CHECKLOOTZ1 = "/lootz";
 SLASH_LISTOFLOOTZ1 = "/lootzlist";
 SLASH_LOOTZHELP1 = "/lootzhelp";
 SLASH_BCKPCKLOOTZ1 = "/lootzbckpck";
+SLASH_ADDLOOTZ1 = "/lootzadd";
+
 SlashCmdList["CHECKLOOTZ"] = function(args)
-	local LegionCurrencies = {1155, 1226, 1342}
-    for i,id in ipairs(LegionCurrencies) do
-        local name, currentAmount = GetCurrencyInfo(id)
-        print(name .. ": " .. currentAmount)
+    if LootzToFollow == nil or LootzToFollow == {} then
+        print("No currencies selected. Use /lootzadd <id> to select currency.")
+    else
+        for i,id in ipairs(LootzToFollow) do
+            local name, currentAmount = GetCurrencyInfo(id)
+            print(name .. ": " .. currentAmount)
+        end
+    end
+end
+
+SlashCmdList["ADDLOOTZ"] = function(args)
+    if LootzToFollow == nil then
+	    LootzToFollow = {}
+	end
+    if args == nil then
+        print("Need ID")
+    else
+        table.insert(LootzToFollow,args)
     end
 end
 
 SlashCmdList["LISTOFLOOTZ"] = function(args)
     local TotalCurrency = GetCurrencyListSize()
     for i=1,TotalCurrency do
-        local _name,_,_,_,_,_count = GetCurrencyListInfo(i)
+        local _name, _, _, _, _, _count = GetCurrencyListInfo(i)
         print(_name .. ": " .. _count)
     end
 end
 
 SlashCmdList["BCKPCKLOOTZ"] = function(args)
-    local TotalCurrency = GetNumWatchedTokens()
-    for i=1,TotalCurrency do
+    local TotalWatchedCurrency = GetNumWatchedTokens()
+    for i=1,TotalWatchedCurrency do
         local foo1, foo2, foo3, foo4, foo5 = GetBackpackCurrencyInfo(i)
 		print(foo1, foo2, foo3, foo4, foo5)
     end
 end
 
 SlashCmdList["LOOTZHELP"] = function(args)
-    print("/lootz: prints selected Legion currencies")
-    print("/lootzlist: prints list of all currencies")
+    print("/lootz: prints selected currencies")
+    print("/lootzadd <id>: add currency to selection")
+    print("/lootzlist: bitch does nothing")
     print("/lootzbckpck: prints id of backpack currencies for /lootz selection")
     print("/lootzhelp: prints this help page")
 
